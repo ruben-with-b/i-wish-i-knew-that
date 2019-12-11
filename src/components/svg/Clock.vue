5240ead3a91c024a84f74b14226e6ef136e627dc
rub-n <ruben.santoro@hs-augsburg.de>
2019-12-11 15:07:02 +0100

    Set and animate clock svg

--- src/components/svg/Clock.vue
+++ src/components/svg/Clock.vue
@@ -1,5 +1,30 @@
 <template>
-  <g fill-rule="evenodd" transform="translate(2 2)">
+  <g id="Uhr" transform="translate(2, 2)">
     <ellipse cx="36" cy="35" rx="36" ry="35"/>
-    <path stroke-linecap="round" stroke-linejoin="round" d="M36 12.151v22.5M46.847 44.278L36 34.651"/></g>
+    <line id="Kleiner-Zeiger" ref="littleHand" stroke-linecap="square" x1="36" y1="35" x2="45.9" y2="48.3"/>
+    <line id="Großer-Zeiger" ref="bigHand" stroke-linecap="square" x1="36" y1="12.1" x2="36" y2="35"/>
+  </g>
 </template>
+
+<script>
+import { TweenMax, Power0 } from 'gsap'
+
+export default {
+	mounted () {
+		const { littleHand, bigHand } = this.$refs
+
+		TweenMax.to(bigHand, 1.5, {
+			rotation: 360,
+			transformOrigin: 'center bottom',
+			repeat: -1,
+			ease: Power0.easeNone
+		})
+
+		TweenMax.to(littleHand, 18, {
+			rotation: 360,
+			repeat: -1,
+			ease: Power0.easeNone
+		})
+	}
+}
+</script>
