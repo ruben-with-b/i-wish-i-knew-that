55c33757b595cfde4613d1b68520b03d0c13f042
rub-n <ruben.santoro@hs-augsburg.de>
2019-12-19 14:59:10 +0100

    Animate desktop & device

--- src/components/svg/Desktop.vue
+++ src/components/svg/Desktop.vue
@@ -3,8 +3,8 @@
         <rect fill="#141414" x="51.1775862" y="92.8240741" width="34.8448276" height="9.98148148" rx="2.58048"></rect>
         <rect fill="#141414" x="2.32931034" y="2.30555556" width="135.5" height="89.9074074"></rect>
         <g transform="translate(14.000000, 15.000000)" fill="#141414">
-            <rect id="fenster" x="1.75" y="19.75" width="108.658621" height="42.5"></rect>
-            <rect id="header" x="1.75" y="1.75" width="108.658621" height="18.5"></rect>
+            <rect ref="window" x="1.75" y="19.75" width="30" height="14"></rect>
+            <rect ref="header" x="1.75" y="1.75" width="30" height="18"></rect>
         </g>
         <g id="Fenster-Icon" transform="translate(22.772414, 21.333333)">
             <path d="M0.397686712,0.561291061 L9.22527292,8.48754499"></path>
@@ -12,3 +12,39 @@
         </g>
     </g>
 </template>
+
+<script>
+import { TimelineMax, Power0 } from 'gsap'
+
+export default {
+	data () {
+		return {
+
+		}
+	},
+	mounted () {
+		const { header, window } = this.$refs
+
+		const tlHeader = new TimelineMax({ repeat: -1, ease: Power0.easeNone })
+		const tlWindow = new TimelineMax({ repeat: -1, ease: Power0.easeNone })
+
+		tlHeader.from(header, 0.5, { width: 30 })
+			.to(header, 0.5, { width: 45 })
+			.to(header, 0.5, { width: 45 })
+			.to(header, 0.5, { width: 70 })
+			.to(header, 0.5, { width: 70 })
+			.to(header, 0.5, { width: 100 })
+			.to(header, 0.5, { width: 100 })
+			.to(header, 0.5, { width: 30 })
+
+		tlWindow.from(window, 0.5, { width: 30, height: 14 })
+			.to(window, 0.5, { width: 45, height: 14 })
+			.to(window, 0.5, { width: 45, height: 24 })
+			.to(window, 0.5, { width: 70, height: 24 })
+			.to(window, 0.5, { width: 70, height: 34 })
+			.to(window, 0.5, { width: 100, height: 34 })
+			.to(window, 0.5, { width: 100, height: 45 })
+			.to(window, 0.5, { width: 30, height: 14 })
+	}
+}
+</script>
--- src/components/svg/DeviceId.vue
+++ src/components/svg/DeviceId.vue
@@ -1,12 +1,42 @@
 <template>
     <g transform="translate(0,0)">
         <g id="Rahmen" fill="#141414">
-            <rect id="Rectangle-Außen" x="1.75" y="1.75" width="109.5" height="74.5"></rect>
-            <rect id="Rectangle-Innen" x="12.75" y="12.75" width="87.5" height="53.5"></rect>
+            <rect x="1.75" y="1.75" width="109.5" height="74.5"></rect>
+            <rect x="12.75" y="12.75" width="87.5" height="53.5"></rect>
         </g>
-        <g id="Desktop" transform="translate(45.000000, 25.500000)">
-            <path d="M1.75,13.25 L1.75,24.75 L22.25,24.75 L22.25,13.25 L1.75,13.25 Z" id="Rectangle"></path>
-            <polygon id="Triangle" points="12 0 24 13 0 13"></polygon>
+        <g>
+            <path ref="building" transform="translate(54, 32) scale(0)" d="M1.75,13.25 L1.75,24.75 L22.25,24.75 L22.25,13.25 L1.75,13.25 Z"></path>
+            <polygon ref="roof" transform="translate(55.25, 32.5) scale(0)" points="12 0 24 13 0 13"></polygon>
         </g>
     </g>
 </template>
+<script>
+import { TweenMax, Bounce } from 'gsap'
+
+export default {
+	data () {
+		return {
+
+		}
+	},
+	mounted () {
+		const { roof, building } = this.$refs
+
+		TweenMax.to(roof, 1.5, {
+			scale: '+=1',
+			transformOrigin: 'center',
+			repeat: -1,
+			repeatDelay: 0.5,
+			ease: Bounce.easeOut
+		})
+
+		TweenMax.to(building, 1.5, {
+			scale: '+=1',
+			transformOrigin: 'center',
+			repeat: -1,
+			repeatDelay: 0.5,
+			ease: Bounce.easeOut
+		})
+	}
+}
+</script>
